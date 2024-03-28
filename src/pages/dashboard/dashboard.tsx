63906c98049fe8400b715ec94faf570c0ef78c5a
Jazyel99 <jaziel@outlook.com>
2024-03-28 11:58:40 -0300

    Atualiza layout da página de dashboard

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,9 +2,15 @@ import Kanban from '@/components/kanban';
 
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div
+      className="grid grid-cols-[260px_1fr_300px] bg-slate-200 h-full"
+    >
+      {/* barra de navegação esquerda */}
+      <div className="bg-blue-500"></div>
+      {/* kanban */}
       <Kanban />
+      {/* barra de detalhes do card */}
+      <div className="bg-blue-200"></div>
     </div>
   );
 }
