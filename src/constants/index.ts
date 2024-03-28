77cc6f16f750d0ee67c7ce778621239fafd065df
Jazyel99 <jaziel@outlook.com>
2024-03-27 21:01:55 -0300

    Adiciona lista de estados no arquivo de constantes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,4 +4,115 @@ export default class Constants {
     'Comércio',
     'Serviços',
   ];
+
+  static readonly LISTA_DE_ESTADOS = [
+    {
+      sigla: 'AC',
+      nome: 'Acre',
+    },
+    {
+      sigla: 'AL',
+      nome: 'Alagoas',
+    },
+    {
+      sigla: 'AP',
+      nome: 'Amapá',
+    },
+    {
+      sigla: 'AM',
+      nome: 'Amazonas',
+    },
+    {
+      sigla: 'BA',
+      nome: 'Bahia',
+    },
+    {
+      sigla: 'CE',
+      nome: 'Ceará',
+    },
+    {
+      sigla: 'DF',
+      nome: 'Distrito Federal',
+    },
+    {
+      sigla: 'ES',
+      nome: 'Espírito Santo',
+    },
+    {
+      sigla: 'GO',
+      nome: 'Goiás',
+    },
+    {
+      sigla: 'MA',
+      nome: 'Maranhão',
+    },
+    {
+      sigla: 'MT',
+      nome: 'Mato Grosso',
+    },
+    {
+      sigla: 'MS',
+      nome: 'Mato Grosso do Sul',
+    },
+    {
+      sigla: 'MG',
+      nome: 'Minas Gerais',
+    },
+    {
+      sigla: 'PA',
+      nome: 'Pará',
+    },
+    {
+      sigla: 'PB',
+      nome: 'Paraíba',
+    },
+    {
+      sigla: 'PR',
+      nome: 'Paraná',
+    },
+    {
+      sigla: 'PE',
+      nome: 'Pernambuco',
+    },
+    {
+      sigla: 'PI',
+      nome: 'Piauí',
+    },
+    {
+      sigla: 'RJ',
+      nome: 'Rio de Janeiro',
+    },
+    {
+      sigla: 'RN',
+      nome: 'Rio Grande do Norte',
+    },
+    {
+      sigla: 'RS',
+      nome: 'Rio Grande do Sul',
+    },
+    {
+      sigla: 'RO',
+      nome: 'Rondônia',
+    },
+    {
+      sigla: 'RR',
+      nome: 'Roraima',
+    },
+    {
+      sigla: 'SC',
+      nome: 'Santa Catarina',
+    },
+    {
+      sigla: 'SP',
+      nome: 'São Paulo',
+    },
+    {
+      sigla: 'SE',
+      nome: 'Sergipe',
+    },
+    {
+      sigla: 'TO',
+      nome: 'Tocantins',
+    },
+  ];
 }
