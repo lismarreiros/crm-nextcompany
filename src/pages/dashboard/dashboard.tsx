acf9aa6df6ef87e357e43d6dfa1d3cff9e99e10c
Jazyel99 <jaziel@outlook.com>
2024-03-28 11:05:46 -0300

    Adicionando componentes iniciais do Kanban.

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,67 +1,10 @@
-import { Button } from '@/components/shadcn/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/shadcn/ui/dialog';
-import { Input } from '@/components/shadcn/ui/input';
-import { Label } from '@/components/shadcn/ui/label';
-
-// product form component
-const ProductFormComponent = () => {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when youre done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
+import Kanban from '@/components/kanban';
 
 export default function DashboardPage() {
   return (
     <div>
       <h1>Dashboard</h1>
-
-      <ProductFormComponent />
-      
+      <Kanban />
     </div>
   );
 }
