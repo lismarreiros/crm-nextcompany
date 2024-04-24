4d87f1921be314c230a52824ad99152d3ff6c1d4
Jazyel99 <jaziel@outlook.com>
2024-04-23 22:23:35 -0300

    refactor: verificando se o retorno é nulo.

--- src/hook/useOportunityFlow.ts
+++ src/hook/useOportunityFlow.ts
@@ -2,13 +2,13 @@ import { OpportunityFlow } from '@/entities/opportunityFlow';
 import { OpportunityFlowService } from '@/services/OpportunityFlowService';
 import { useEffect, useState } from 'react';
 export function useOpportunityFlow() {
-  const [opportunityFlows, setOpportunityFlows] = useState<OpportunityFlow[] | null>([]);
+  const [opportunityFlows, setOpportunityFlows] = useState<OpportunityFlow[]>([]);
   const [errorFetchOpportunityFlows, setErrorFetchOpportunityFlows] = useState(false);
   useEffect(() => {
     const fetchOpportunityFlows = async () => {
       try {
         const response = await OpportunityFlowService.getOpportunityFlow();
-        setOpportunityFlows(response);
+        if (response) setOpportunityFlows(response);
       } catch (error) {
         console.log(error);
         
