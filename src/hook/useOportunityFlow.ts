901b0fabf666b3d9b03d9ce1f3f8b9f5451283e9
Jazyel99 <jaziel@outlook.com>
2024-04-24 10:59:03 -0300

    feat: adionado metedo no hook para fazer chamada do metodo do service.

--- src/hook/useOportunityFlow.ts
+++ src/hook/useOportunityFlow.ts
@@ -1,9 +1,22 @@
 import { OpportunityFlow } from '@/entities/opportunityFlow';
+import { OpportunityFlowWithBussiness } from '@/entities/opportunityFlowWithBussiness';
 import { OpportunityFlowService } from '@/services/OpportunityFlowService';
 import { useEffect, useState } from 'react';
 export function useOpportunityFlow() {
   const [opportunityFlows, setOpportunityFlows] = useState<OpportunityFlow[]>([]);
+  const [opportunityFlowsWithBussiness, setOpportunityFlowsWithBussiness] = useState<OpportunityFlowWithBussiness[]>([]);
   const [errorFetchOpportunityFlows, setErrorFetchOpportunityFlows] = useState(false);
+
+  const getAllOpportunityFlowWithBussiness = async () => {
+    try {
+      const response = await OpportunityFlowService.getOpportunityFlowWithBussiness();
+      if (response) setOpportunityFlowsWithBussiness(response);
+    } catch (error) {
+      console.log(error);
+      // setErrorFetchOpportunityFlows(true);
+    }
+  };
+
   useEffect(() => {
     const fetchOpportunityFlows = async () => {
       try {
@@ -17,7 +30,8 @@ export function useOpportunityFlow() {
     };
 
     fetchOpportunityFlows();
+    getAllOpportunityFlowWithBussiness();
   }, []);
 
-  return { opportunityFlows, errorFetchOpportunityFlows };
+  return { opportunityFlows, errorFetchOpportunityFlows, opportunityFlowsWithBussiness};
 }
