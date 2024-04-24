81f1f01d8d160e30ec3058dd0949b018d84fc4f8
Jazyel99 <jaziel@outlook.com>
2024-04-24 10:57:57 -0300

    feat: adionando metodo para trazer fluxos de oportunidade com negócios.

--- src/services/OpportunityFlowService.ts
+++ src/services/OpportunityFlowService.ts
@@ -1,16 +1,20 @@
 import { OpportunityFlow } from '@/entities/opportunityFlow';
 import axios from 'axios';
+import Constants from '@/constants';
+import { OpportunityFlowWithBussiness } from '@/entities/opportunityFlowWithBussiness';
 
 export class OpportunityFlowService {
+
+  private static api = axios.create({
+    baseURL: Constants.BASE_URL,
+  });
+
   public static async getOpportunityFlow(): Promise<OpportunityFlow[] | null> {
-    const api = axios.create({
-      baseURL: 'http://localhost:3001',
-    });
     // axios call
-    const response = await api.get<any>('/opportunity-flow', {
+    const response = await this.api.get<any>('/opportunity-flow', {
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmV4dGNvbXBhbnkiLCJjb21wYW55SWQiOjEsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTcxMzkxNTEyNiwiZXhwIjoxNzE0MDAxNTI2fQ.YR6mHYAiZFgFQPzk5Ce31ELFJDwT-Q7mBuYQbXTF7og',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmV4dGNvbXBhbnkiLCJjb21wYW55SWQiOjEsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTcxMzk1Nzg3MCwiZXhwIjoxNzE0MDQ0MjcwfQ.hjL49a3SO2UR0SY9xcRYX9GaEiuaVefOAMX1FfWO0Qo',
       },
     });
 
@@ -19,6 +23,35 @@ export class OpportunityFlowService {
         id: data.id,
         description: data.description,
         order: data.order,
+      }));
+    }
+
+    return null;
+  }
+
+  public static async getOpportunityFlowWithBussiness(): Promise<OpportunityFlowWithBussiness[] | null> {
+    // axios call
+    const response = await this.api.get<any>('/opportunity-flow-with-bussiness', {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmV4dGNvbXBhbnkiLCJjb21wYW55SWQiOjEsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTcxMzk1Nzg3MCwiZXhwIjoxNzE0MDQ0MjcwfQ.hjL49a3SO2UR0SY9xcRYX9GaEiuaVefOAMX1FfWO0Qo',
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data.map((data: any) => ({
+        id: data.id,
+        description: data.description,
+        order: data.order,
+        bussiness: data.bussiness.map((bussiness: any) => ({
+          id: bussiness.id,
+          description: bussiness.description,
+          status: bussiness.status,
+          closedAt: bussiness.closedAt,
+          startedAt: bussiness.startedAt,
+          prevision: bussiness.prevision,
+          clientId: bussiness.clientId,
+        }))
       }));
     }
 
