aadbfebdd14d1f4216bfbc42555a25ed46012250
Jazyel99 <jaziel@outlook.com>
2024-04-26 15:17:15 -0300

    style: lendo token do localstorage

--- src/services/BussinessService.ts
+++ src/services/BussinessService.ts
@@ -7,7 +7,7 @@ export class BussinessService {
     baseURL: Constants.BASE_URL,
   });
 
-  private static token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmV4dGNvbXBhbnkiLCJjb21wYW55SWQiOjEsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTcxNDA3MjYyNiwiZXhwIjoxNzE0MTU5MDI2fQ.qX2L_BjiE2BCwjCITeQ-fAvI0kYrYewp_5GqZ4XZlZU';
+  private static token = localStorage.getItem('APP_ACCESS_TOKEN')?.replace(/"/g, '');
 
   public static async getBussinesById(bussinessId: number): Promise<Bussiness| null> {
 
--- src/services/OpportunityFlowService.ts
+++ src/services/OpportunityFlowService.ts
@@ -10,7 +10,7 @@ export class OpportunityFlowService {
     baseURL: Constants.BASE_URL,
   });
 
-  private static token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmV4dGNvbXBhbnkiLCJjb21wYW55SWQiOjEsInJvbGVzIjpbImFkbWluIl0sImlhdCI6MTcxNDA3MjYyNiwiZXhwIjoxNzE0MTU5MDI2fQ.qX2L_BjiE2BCwjCITeQ-fAvI0kYrYewp_5GqZ4XZlZU';
+  private static token = localStorage.getItem('APP_ACCESS_TOKEN')?.replace(/"/g, '');
 
   public static async getOpportunityFlow(): Promise<OpportunityFlow[] | null> {
     // axios call
